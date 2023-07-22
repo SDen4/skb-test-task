bcbea9b8a4a17b58d37973a36791ea6780230276
SDen4 <asw325@yandex.ru>
2023-07-22 15:26:07 +0500

    v.1.8.0. deploy to github pages, delete mock data from store

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.3.
 
+---
+
+## Deploy to github pages
+
+[Инструкция](https://angular.io/guide/deployment#deploy-to-github-pages) для деплоя на github pages в документации Angular
+
+---
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
--- src/app/store/state/tasks.state.ts
+++ src/app/store/state/tasks.state.ts
@@ -1,33 +1,3 @@
 import { ITask } from 'src/app/model';
 
-export const initTaskListState: ITask[] = [
-  {
-    id: 123,
-    title: 'Test task red',
-    description: 'Description of the test task (Test task red)...',
-    deadline: '2021-03-03',
-    isDone: false,
-  },
-  {
-    id: 124,
-    title: 'Test task red',
-    description: 'Description of the test task (Test task orage)...',
-    deadline: '2023-07-22',
-    isDone: false,
-  },
-  {
-    id: 125,
-    title: 'Test task white',
-    description: 'Description of the test task (Test task white)...',
-    deadline: '2023-07-27',
-    isDone: false,
-  },
-  {
-    id: 126,
-    title: 'Test task orange and long long teeeeeeexxxxxxxxxxxxxxtttttttttttt',
-    description:
-      'Description of the test task (Test task white)...dskjfhsadkjfhksadjhfksdjhfkjsdfkhsd dkfsdk dsfskdj dsfjhskd sdjfhkasd ksdjhf ksd  ksdjfhk sd  kdjfk sdf skdjfh skdj',
-    deadline: '2023-07-24',
-    isDone: false,
-  },
-];
+export const initTaskListState: ITask[] = [];
